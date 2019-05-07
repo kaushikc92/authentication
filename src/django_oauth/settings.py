c1b2df6db29ae110f01fc814861336f3376e5dfb
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2019-05-07 17:38:02 +0000

    Updated create user form

--- src/django_oauth/settings.py
+++ src/django_oauth/settings.py
@@ -83,6 +83,10 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 WSGI_APPLICATION = 'django_oauth.wsgi.application'
 
 
@@ -128,9 +132,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
