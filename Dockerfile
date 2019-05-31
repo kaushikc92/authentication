1dfc0246ad066977c5bfb6841e71174ba6bd7896
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2019-05-31 00:43:18 +0000

    Persistent Postgres Database

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,5 @@ RUN pip install --trusted-host pypi.python.org -r requirements.txt
 COPY ./src/ /code/
 
 EXPOSE 8000
-CMD python manage.py runserver 0.0.0.0:8000
+CMD python manage.py migrate && python manage.py runserver 0.0.0.0:8000
+#CMD ["sh", "-c", "/bin/bash"]
--- src/django_oauth/settings.py
+++ src/django_oauth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xm(q_&2q7m-$3m=of)^40nis1vo2w-^896^04^k-isx7o@-_o@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-DEBUG_LOCAL = True
+DEBUG_LOCAL = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -97,8 +97,11 @@ WSGI_APPLICATION = 'django_oauth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432,
     }
 }
 
